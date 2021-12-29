095ba4f69e4776be66cdac069ce3c26e75992cdc
shehanbandara <s2bandara@uwaterloo.ca>
2021-12-29 00:18:50 -0500

    Created requests to GET, POST, DELETE, and PUT
    Created requests to GET, POST, DELETE, and PUT

--- api/server.js
+++ api/server.js
@@ -13,4 +13,33 @@ mongoose.connect('mongodb://127.0.0.1:27017/ToDoApp', {
     useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
+// GET ToDo list
+app.get('/todolist', async (req, res) => {
+    const todolist = await ToDo.find();
+    res.json(todolist);
+});
+
+// Add item to ToDo list
+app.post('/todo/new', (req, res) => {
+    const todo = new ToDo({
+        text: req.body.text
+    });
+    todo.save();
+    res.json(todo);
+});
+
+// Delete item from ToDo list
+app.delete('/todo/delete/:id', async (req, res) => {
+    const deletetodo = await ToDo.findByIdAndDelete(req.params.id);
+    res.json(deletetodo);
+});
+
+// Change completion status of item in ToDo list
+app.put('/todo/update/:id', async (req, res) => {
+    const todo = await ToDo.findById(req.params.id);
+    todo.complete = !todo.complete;
+    todo.save();
+    res.json(todo);
+});
+
 app.listen(3001, () => console.log("Server running on Port 3001"));

7fc540186237f8d88b0eb1684918c66f122d5dad
shehanbandara <s2bandara@uwaterloo.ca>
2021-12-29 22:54:23 -0500

    Continued working on front end
    Continued working on front end

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -19,14 +19,29 @@ const ToDoList = () => {
 			.catch((err) => console.error("Error: ", err));
 	}
 
+    const DeleteToDo = async id => {
+		const data = await fetch(APIBASE + '/todo/delete/' + id, { method: "DELETE" }).then(res => res.json());
+		setToDoList(toDoList => toDoList.filter(todo => todo._id !== data._id));
+	}
+
+    const CompleteToDo = async id => {
+        const data = await fetch(APIBASE + '/todo/update/' + id, { method: "PUT" }).then(res => res.json());
+        setToDoList(toDoList => toDoList.map(todo => {
+            if (todo._id === data._id) {
+                todo.complete = data.complete;
+            }
+            return todo;
+        }));
+	}
+
     return (
         <>
             <div className="ToDoList">
                 {toDoList.map(todo => (
-                    <div className="todo" key={todo._id}>
+                    <div className={"todo " + (todo.complete ? "is-complete" : "")} key={todo._id}>
                         <div className="box"></div>
-                        <div className="text">{ todo.text }</div>
-                        <div className="delete">x</div>
+                        <div className="text" onClick={() => CompleteToDo(todo._id)}>{ todo.text }</div>
+                        <div className="delete" onClick={() => DeleteToDo(todo._id)}>X</div>
                     </div>
                 ))}
             </div>
